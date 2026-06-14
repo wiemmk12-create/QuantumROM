fda40e1ad3dd78d4ebc1ed137b652cac51e89153
wiemmk12-create <wiemmk12@gmail.com>
2026-06-14 17:55:52 +0100

    Update install_packages.sh

--- scripts/install_packages.sh
+++ scripts/install_packages.sh
@@ -11,19 +11,19 @@ install_packages() {
     if [ "$OS" = "ubuntu" ] || [ "$OS" = "debian" ]; then
         echo "Installing for Ubuntu/Debian..."
         sudo apt update
-		sudo apt install -y p7zip-full lz4 liblz4-1 liblz4-dev libzstd1 libzstd-dev build-essential android-sdk-libsparse-utils f2fs-tools fuse2fs fuse e2fsprogs python3 python3-pip zipalign unzip openjdk-17-jdk jq perl xxd kmod erofs-utils
+		sudo apt install -y p7zip-full lz4 liblz4-1 liblz4-dev libzstd1 libzstd-dev build-essential android-sdk-libsparse-utils f2fs-tools fuse2fs fuse e2fsprogs python3 python3-pip zipalign unzip openjdk-21-jdk jq perl xxd kmod erofs-utils
 		sudo apt autoremove -y
 
     # Arch Linux
     elif [ "$OS" = "arch" ]; then
         echo "Installing for Arch Linux..."
-        pacman -Syu --noconfirm p7zip lz4 zstd base-devel android-tools f2fs-tools fuse2 fuse3 e2fsprogs python python-pip zip unzip jdk17-openjdk jq perl xxd kmod erofs-utils
+        pacman -Syu --noconfirm p7zip lz4 zstd base-devel android-tools f2fs-tools fuse2 fuse3 e2fsprogs python python-pip zip unzip jdk21-openjdk jq perl xxd kmod erofs-utils
 
     # Fedora
     elif [ "$OS" = "fedora" ]; then
         echo "Installing for Fedora..."
         sudo dnf upgrade -y
-		sudo dnf install -y p7zip p7zip-plugins lz4 lz4-devel zstd zstd-devel gcc gcc-c++ make android-tools f2fs-tools fuse fuse-libs e2fsprogs python3 python3-pip zip unzip java-17-openjdk-devel jq perl vim-common kmod erofs-utils
+		sudo dnf install -y p7zip p7zip-plugins lz4 lz4-devel zstd zstd-devel gcc gcc-c++ make android-tools f2fs-tools fuse fuse-libs e2fsprogs python3 python3-pip zip unzip java-21-openjdk-devel jq perl vim-common kmod erofs-utils
     else
         echo "Unsupported OS: $OS"
         return 1
